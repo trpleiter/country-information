3381bbee710ccdd00c2a2fdb8ef444b82d216793
Tom Pleiter <you@example.com>
2022-02-04 10:53:08 +0100

    zo goed als af, alleen hoofdstuk 8.2 nog even doornemen

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
-console.log('Hallo daar!');
+import axios from "axios";
+
+const infoCountriesElement = document.getElementById("information-countries");
+
+async function getCountryInfo() {
+    try {
+        const resultCountryInfo = await axios.get('https://restcountries.com/v2/all');
+        resultCountryInfo.data.sort((a, b) => {
+            return a.population - b.population;
+        })
+        resultCountryInfo.data.map((countryInfo) => {
+            infoCountriesElement.innerHTML +=
+                `<div class="country-info">
+                 <li class="${countryInfo.region}"><img src="${countryInfo.flag}"</img> ${countryInfo.name}</li>
+                 <li>has a population of 
+                     ${countryInfo.population} people</li>
+                 </div>`
+        })
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getCountryInfo();
+
+
